5a4d377d785481d84522138d852f94979097f999
fayis <fayispvchelari@gmail.com>
2023-10-12 11:24:24 +0530

    custom_user

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser,AbstractBaseUser
 # Create your models here.
 
+class CustomeUser(AbstractUser,AbstractBaseUser):
+    pass
 
 class Category(models.Model):
     name = models.CharField( max_length=50)
