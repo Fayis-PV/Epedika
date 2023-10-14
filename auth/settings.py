2bd48f111b1836359c6230d10b431920cb1f5ad3
fayis <fayispvchelari@gmail.com>
2023-10-14 07:36:16 +0530

    debug resetted

--- auth/settings.py
+++ auth/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f5b@dkzcr6qjmtu=x@2motgz)4zr&_pjps%e2n0=fkteiw^c8!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
